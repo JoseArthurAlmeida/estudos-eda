852bf6ec5e2cdbc0ed15507b7ba0d98611b0743c
José Arthur <ja1364091@gmail.com>
2025-09-24 14:13:25 -0300

    Adicionando Comentários no BubbleSort

--- src/algoritmosOrdenacao/BubbleSort.java
+++ src/algoritmosOrdenacao/BubbleSort.java
@@ -11,12 +11,26 @@ public class BubbleSort {
     }
 
     public static List<Integer> bubbleSort(List<Integer> lista) {
+        // Primeiro loop: Responsável por controlar o número de passos necessários.
+        // Em cada passe, o maior elemento "flutua" para sua posição correta no final da parte não ordenada.
         for (int i = 0; i < lista.size(); i++) {
+/*
+             Segundo loop: Percorre a parte não ordenada da lista para realizar as comparações e trocas.
+             O 'j' começa do final da lista e vai diminuindo.
+             Ele para quando 'j' for igual a 'i'
+             Portanto, o loop interno foca apenas nos elementos da parte desordenada da lista, que vai do índice 0 até 'i - 1'.
+*/
             for (int j = lista.size() - 1; j > i; j--) {
+                // Compara o elemento atual (j-1) com o seu próximo elemento (j).
                 if (lista.get(j - 1) > lista.get(j)) {
-                    int aux = lista.get(j - 1);
-                    lista.set(j - 1, lista.get(j));
-                    lista.set(j , aux);
+                    // Se o elemento anterior for maior que o elemento atual, eles estão na ordem errada.
+                    // Realiza a troca dos elementos.
+                    int maior = lista.get(j - 1);
+                    int menor = lista.get(j);
+                    // Define o elemento anterior com o valor do elemento atual.
+                    lista.set(j - 1, menor);
+                    // Define o elemento atual com o valor armazenado em 'maior' (o valor original do elemento anterior).
+                    lista.set(j, maior);
                 }
             }
         }
