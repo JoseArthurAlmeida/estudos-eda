9187c120c04736da4c06e75aa3b922f7cf07d9db
José Arthur <ja1364091@gmail.com>
2026-01-09 19:41:34 -0300

    Adicionando testes do método buscar

--- src/arvoreB/Main.java
+++ src/arvoreB/Main.java
@@ -14,5 +14,8 @@ public class Main {
 
         System.out.println("\n--- RESULTADO FINAL ---");
         arvore.imprimir();
+
+        System.out.println("Buscar '10': " + arvore.buscar(10));
+        System.out.println("Buscar '12': " + arvore.buscar(12));
     }
 }
