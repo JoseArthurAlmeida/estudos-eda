d86ba37aba9f07efedc935f83a566aa62e355736
José Arthur <ja1364091@gmail.com>
2026-01-09 19:56:58 -0300

    Adicionando testes para o método de remoção na ÁrvoreB

--- src/arvoreB/Main.java
+++ src/arvoreB/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         ArvoreB arvore = new ArvoreB();
 
-        int[] valores = {5, 10, 15, 20, 25, 30, 35, 40, 45, 50};
+        int[] valores = {40, 30, 80, 100, 90, 81, 82, 83, 10, 110};
 
         for (int valor : valores) {
             System.out.println("Inserindo: " + valor);
@@ -15,7 +15,23 @@ public class Main {
         System.out.println("\n--- RESULTADO FINAL ---");
         arvore.imprimir();
 
+        System.out.println("\n TESTES DE BUSCA");
         System.out.println("Buscar '10': " + arvore.buscar(10));
         System.out.println("Buscar '12': " + arvore.buscar(12));
+
+        System.out.println("\n--- Árvore Original ---");
+        arvore.imprimir();
+
+        System.out.println("\n--- Removendo 110 (Simples) ---");
+        arvore.remover(110);
+        arvore.imprimir();
+
+        System.out.println("\n--- Removendo 82 (Simples) ---");
+        arvore.remover(82);
+        arvore.imprimir();
+
+        System.out.println("\n--- Removendo 83 (Causa Fusão/Merge) ---");
+        arvore.remover(83);
+        arvore.imprimir();
     }
 }
